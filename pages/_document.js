f8394561620e84c2b2d4e562844ef2c604753a6b
Nada Emara <doooya@me.com>
2026-04-10 06:52:04 +0100

    Add Google Analytics G-0D5B9X6HGZ

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
       <Head>
         <meta name="google-adsense-account" content="ca-pub-5906025502715825" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5906025502715825" crossOrigin="anonymous" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WSEVLTNRVR" />
-        <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WSEVLTNRVR');`}} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0D5B9X6HGZ" />
+        <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-0D5B9X6HGZ');`}} />
       </Head>
       <body>
         <Main />
